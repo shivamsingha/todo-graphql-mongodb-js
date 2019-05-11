c7b0478052b8eb83be9029334f089c1edc8d5ea2
shivamsingha <shivamgit@protonmail.com>
2019-05-11 10:26:34 +0530

    Fix 'Topology was destroyed' error
    Signed-off-by: shivamsingha <shivamgit@protonmail.com>

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,26 @@ const { GraphQLServer } = require('graphql-yoga')
 const cookieParser = require('cookie-parser')
 
 const { client, dbName } = require('./mongo')
-const config = require('./graphqlConfig')
-const resolvers = config.resolvers;
+const { schema, options, resolvers } = require('./graphqlConfig')
+
+process.on('exit', (code) => {
+    client.close()
+    console.log(`Exiting with code ${code}`)
+})
 
 (async function () {
     try {
         await client.connect()
         const db = client.db(dbName)
         const server = new GraphQLServer({
-            typeDefs: config.schema,
+            typeDefs: schema,
             resolvers,
             context: (req) => ({ ...req, db })
         })
         server.express.use(cookieParser())
-        server.start(config.options, ({ port }) => console.log(`Server is running on http://localhost:${port}`))
+        server.start(options, ({ port }) => console.log(`Server is running on http://localhost:${port}`))
     }
     catch (err) {
         console.error(err.stack)
     }
-
-    client.close()
 })()
